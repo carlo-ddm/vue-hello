4b0b4ed26db321757357e3eb8af122c04a254f4c
carlo-ddm <carloduminuco90@gmail.com>
2022-05-02 16:40:00 +0200

    esercizio con bonus

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+// **Descrizione:**
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// **Bonus:**
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
 const app = new Vue ({
 
   el: '#app',
 
   data:{
-    messaggio: 'Hello World!'
+    messaggio: 'Hello World!',
+    img: 'https://image.spreadshirtmedia.net/image-server/v1/mp/compositions/T773A1MPA1611PT10X29Y22D189000786FS782/views/3,width=550,height=550,appearanceId=1,backgroundColor=FFFFFF,noPt=true/hello-world-nel-linguaggio-di-programmazione-piet-tazza-termica.jpg'
   }
 });
